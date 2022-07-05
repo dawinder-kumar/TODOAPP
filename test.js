e5da87f6ed87cea7bff61e031289a67ee85b8f03
dawinder <kumardawinder38@gmail.com>
2022-07-05 09:28:36 +0530

    count  added

--- test.js
+++ test.js
@@ -2,10 +2,13 @@ let input = document.getElementById("input");
 let button = document.getElementById("add-btn");
 let todoUl = document.getElementById('ul');
 let inprogressUl = document.querySelector("#box2-ul");
+let count = document.getElementById("count");
+let i = 0;
+
 
 button.addEventListener('click', function () {
-
-
+    i+=1;
+    count.innerHTML=i;
     if (input.value == "") {
         alert("Please enter some text");
     } else {
@@ -14,11 +17,14 @@ button.addEventListener('click', function () {
         content = document.createTextNode(input.value);
         todoList.append(content);
         todoUl.append(todoList);
-
+        todoList.style.background = 'white';
+        todoList.style.borderRadius = '1rem';
+    
 
         let taskAdd = document.createElement("button");
         taskAdd.append(document.createTextNode("addTask"));
         todoList.append(taskAdd);
+        input.value = "";
         taskAdd.addEventListener('click', function () {
             let inprogressList = document.createElement("li");
             let taskDone = document.createElement("button");
@@ -28,27 +34,28 @@ button.addEventListener('click', function () {
             todoList.append(taskDone);
             taskAdd.style.display = 'none';
 
+
             taskDone.addEventListener('click', function () {
                 completedUl.append(inprogressList);
-
                 taskDone.style.display = 'none';
+            });
+
+             let doneButton = document.getElementById("done-button");
+             let completedUl = document.getElementById('box3-ul');
+            doneButton.addEventListener('click', function () {
+                let completedList = document.createElement("li");
+                completedList.append(inprogressUl);
+                completedUl.append(completedList);
 
             });
         });
-        input.value = "";
-        let doneButton = document.getElementById("done-button");
-        let completedUl = document.getElementById('box3-ul')
-        doneButton.addEventListener('click', function () {
-            let completedList = document.createElement("li");
-            completedList.append(inprogressUl);
-            completedUl.append(completedList);
-        });
-        let clear1 = document.createElement("button");
-        clear1.append(document.createTextNode("clear"));
-        todoList.append(clear1);
-        clear1.addEventListener('click', function () {
+        let clear = document.createElement("button");
+        clear.append(document.createTextNode("clear"));
+        todoList.append(clear);
+        clear.addEventListener('click', function () {
             alert("Are you really Want to Delete Task");
             todoList.remove();
+
         })
     }
 });
